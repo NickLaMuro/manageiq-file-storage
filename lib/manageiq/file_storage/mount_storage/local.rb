ca4dafde149b31e8fee157e3640ba096d74bab3a
Nick LaMuro <nicklamuro@gmail.com>
2020-01-25 12:22:15 -0600

    Properly namespace all moved classes

--- lib/manageiq/file_storage/mount_storage/local.rb
+++ lib/manageiq/file_storage/mount_storage/local.rb
@@ -1,28 +1,35 @@
-require 'util/mount/miq_generic_mount_session'
+require 'manageiq/file_storage/mount_storage'
 
-# MiqLocalMountSession is meant to be a representation of the local file system
-# that conforms to the same interface as MiqLocalMountSession (and by proxy,
-# MiqFileSystem::Interface).
+# ManageIQ::FileStorage::MountStorage::Local is meant to be a representation of
+# the local file system that conforms to the same interface as
+# ManageIQ::FileStorage::MountStorage (and by proxy,
+# ManageIQ::FileStorage::Interface).
 #
-# See MiqGenericMountSession for info on methods available.
-class MiqLocalMountSession < MiqGenericMountSession
-  def self.uri_scheme
-    "file".freeze
-  end
+# See ManageIQ::FileStorage::MountStorage for info on methods available.
+module ManageIQ
+  class FileStorage
+    class MountStorage
+      class Local < ManageIQ::FileStorage::MountStorage
+        def self.uri_scheme
+          "file".freeze
+        end
 
-  # no-op these since they are not relavent to the local file system
-  #
-  # rubocop:disable Style/SingleLineMethods, Layout/EmptyLineBetweenDefs
-  def connect; end      # :nodoc:
-  def disconnect; end   # :nodoc:
-  def mount_share; end  # :nodoc:
-  # rubocop:enable Style/SingleLineMethods, Layout/EmptyLineBetweenDefs
+        # no-op these since they are not relavent to the local file system
+        #
+        # rubocop:disable Style/SingleLineMethods, Layout/EmptyLineBetweenDefs
+        def connect; end      # :nodoc:
+        def disconnect; end   # :nodoc:
+        def mount_share; end  # :nodoc:
+        # rubocop:enable Style/SingleLineMethods, Layout/EmptyLineBetweenDefs
 
-  def relative_to_mount(remote_file) # :nodoc:
-    remote_file
-  end
+        def relative_to_mount(remote_file) # :nodoc:
+          remote_file
+        end
 
-  def uri_to_local_path(remote_file) # :nodoc:
-    File.expand_path(remote_file)
+        def uri_to_local_path(remote_file) # :nodoc:
+          File.expand_path(remote_file)
+        end
+      end
+    end
   end
 end
--- lib/manageiq/file_storage/mount_storage/nfs.rb
+++ lib/manageiq/file_storage/mount_storage/nfs.rb
@@ -1,41 +1,47 @@
-require 'util/mount/miq_generic_mount_session'
+require 'manageiq/file_storage/mount_storage'
 
-class MiqNfsSession < MiqGenericMountSession
-  PORTS = [2049, 111]
+module ManageIQ
+  class FileStorage
+    class MountStorage
+      class NFS < ManageIQ::FileStorage::MountStorage
+        PORTS = [2049, 111]
 
-  def self.uri_scheme
-    "nfs".freeze
-  end
+        def self.uri_scheme
+          "nfs".freeze
+        end
 
-  def initialize(log_settings)
-    super(log_settings.merge(:ports => PORTS))
-  end
+        def initialize(log_settings)
+          super(log_settings.merge(:ports => PORTS))
+        end
 
-  def connect
-    scheme, userinfo, @host, port, registry, @mount_path, opaque, query, fragment = URI.split(URI.encode(@settings[:uri]))
-    super
-  end
+        def connect
+          scheme, userinfo, @host, port, registry, @mount_path, opaque, query, fragment = URI.split(URI.encode(@settings[:uri]))
+          super
+        end
 
-  def mount_share
-    super
+        def mount_share
+          super
 
-    log_header = "MIQ(#{self.class.name}-mount_share)"
-    logger.info("#{log_header} Connecting to host: [#{@host}], share: [#{@mount_path}] using mount point: [#{@mnt_point}]...")
-    # URI: nfs://192.168.252.139/exported/miq
-    # mount 192.168.252.139:/exported/miq /mnt/miq
+          log_header = "MIQ(#{self.class.name}-mount_share)"
+          logger.info("#{log_header} Connecting to host: [#{@host}], share: [#{@mount_path}] using mount point: [#{@mnt_point}]...")
+          # URI: nfs://192.168.252.139/exported/miq
+          # mount 192.168.252.139:/exported/miq /mnt/miq
 
-    mount = "mount"
-    mount << " -r" if settings_read_only?
+          mount = "mount"
+          mount << " -r" if settings_read_only?
 
-    # Quote the host:exported directory since the directory can have spaces in it
-    case Sys::Platform::IMPL
-    when :macosx
-      runcmd("sudo #{mount} -t nfs -o resvport '#{@host}:#{@mount_path}' #{@mnt_point}")
-    when :linux
-      runcmd("#{mount} '#{@host}:#{@mount_path}' #{@mnt_point}")
-    else
-      raise "platform not supported"
+          # Quote the host:exported directory since the directory can have spaces in it
+          case Sys::Platform::IMPL
+          when :macosx
+            runcmd("sudo #{mount} -t nfs -o resvport '#{@host}:#{@mount_path}' #{@mnt_point}")
+          when :linux
+            runcmd("#{mount} '#{@host}:#{@mount_path}' #{@mnt_point}")
+          else
+            raise "platform not supported"
+          end
+          logger.info("#{log_header} Connecting to host: [#{@host}], share: [#{@mount_path}]...Complete")
+        end
+      end
     end
-    logger.info("#{log_header} Connecting to host: [#{@host}], share: [#{@mount_path}]...Complete")
   end
 end
