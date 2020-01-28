522aa8e28bcdf0a96ab65e1c5d0a2cf6a426cb7a
Nick LaMuro <nicklamuro@gmail.com>
2020-01-28 17:38:05 -0600

    Add FTPSpecMatcherHelper
    Used for shared methods within the FTP based custom_matchers for shared
    methods across the two of them.

--- spec/support/custom_matchers/exist_on_ftp_server.rb
+++ spec/support/custom_matchers/exist_on_ftp_server.rb
@@ -1,8 +1,12 @@
+require "support/ftp_spec_matcher_helper"
+
 # Assumes this in run in a :with_ftp_server context so an FTP server on
 # localhost is available.
 #
 # See spec/support/with_ftp_server.rb for more info.
 RSpec::Matchers.define :exist_on_ftp_server do
+  include FTPSpecMatcherHelper
+
   match do |actual|
     !list_in_ftp(actual).empty?
   end
@@ -15,25 +19,6 @@ RSpec::Matchers.define :exist_on_ftp_server do
     fail_msg(actual, :negated => true)
   end
 
-  def with_connection
-    Net::FTP.open("localhost") do |ftp|
-      ftp.login("ftpuser", "ftppass")
-      yield ftp
-    end
-  end
-
-  # Do searches with Net::FTP instead of normal directory scan (even though we
-  # could) just so we are exercising the FTP interface as expected.
-  def list_in_ftp(file_or_dir)
-    with_connection do |ftp|
-      begin
-        ftp.nlst(to_path_string(file_or_dir))
-      rescue Net::FTPPermError
-        []
-      end
-    end
-  end
-
   def fail_msg(actual, negated: false)
     dir     = File.dirname(actual)
     entries = list_in_ftp(dir)
@@ -44,9 +29,5 @@ RSpec::Matchers.define :exist_on_ftp_server do
       Entries for #{dir}:
       #{entries.empty? ? "  []" : entries.map { |e| "  #{e}" }.join("\n")}
     MSG
-  end
-
-  def to_path_string(path)
-    path.try(:path) || URI.split(path.to_s)[5]
   end
 end
--- spec/support/custom_matchers/have_size_on_ftp_server_of.rb
+++ spec/support/custom_matchers/have_size_on_ftp_server_of.rb
@@ -1,30 +1,29 @@
+require "support/ftp_spec_matcher_helper"
+
 # Assumes this in run in a :with_ftp_server context so an FTP server on
 # localhost is available.
 #
 # See spec/support/with_ftp_server.rb for more info.
 RSpec::Matchers.define :have_size_on_ftp_server_of do |expected|
+  include FTPSpecMatcherHelper
+
   match do |filepath|
     size = size_on_ftp(filepath)
     size == expected
   end
 
-  def with_connection
-    Net::FTP.open("localhost") do |ftp|
-      ftp.login("ftpuser", "ftppass")
-      yield ftp
-    end
+  failure_message do |actual|
+    fail_msg(actual)
   end
 
-  # Do searches with Net::FTP instead of normal directory scan (even though we
-  # could) just so we are exercising the FTP interface as expected.
-  def size_on_ftp(file_or_dir)
-    path = file_or_dir.try(:path) || URI.split(file_or_dir.to_s)[5]
-    with_connection do |ftp|
-      begin
-        ftp.size(path)
-      rescue Net::FTPPermError
-        0
-      end
-    end
+  def fail_msg(actual, negated: false)
+    dir     = File.dirname(actual)
+    entries = list_in_ftp(dir).map { |filename| [filename, size_on_ftp(filename)] }
+    <<~MSG
+      expected: #{to_path_string(actual)} to be of size #{expected}"
+
+      Entries for #{dir}:
+      #{entries.empty? ? "  []" : entries.map { |f, s| "  #{f}: #{s}" }.join("\n")}
+    MSG
   end
 end
